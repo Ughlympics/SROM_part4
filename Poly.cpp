a28853d3bbc9d785f45fe44a2695a7519763bdf1
Kirill Radkevich <Ughlympics>
2024-12-29 22:03:54 +0200

    Mul workin

--- Poly.cpp
+++ Poly.cpp
@@ -1,6 +1,7 @@
 #include "Poly.h"
 #include <array>
 #include <iostream>
+#include <fstream>
 
 
 std::bitset<Poly::_POWER> Poly::mult_matrix[Poly::_POWER];
@@ -27,12 +28,29 @@ void Poly::generate_mult_matrix(static std::bitset<_POWER> mult_matrix[_POWER]) 
             else {
                 mult_matrix[i][j] = 0;
             }
+            //std::cout << mult_matrix[i][j] ;
         }
+        //std::cout <<  std::endl;
     }
+
+    /*std::ofstream file("mult_matrix.txt");
+    if (file.is_open()) {
+        for (int i = 0; i < _POWER; ++i) {
+            for (int j = 0; j < _POWER; ++j) {
+                file << mult_matrix[i][j];
+            }
+            file << '\n';
+        }
+        file.close(); 
+        std::cout << "Matrix saved to mult_matrix.txt" << std::endl;
+    }
+    else {
+        std::cerr << "Unable to open file for writing!" << std::endl;
+    }*/
     multiplication_matrix = true;
 }
 
-void Poly::print_mult_matrix(static std::bitset<_POWER> mult_matrix[_POWER]) {
+void Poly::print_matrix(static std::bitset<_POWER> mult_matrix[_POWER]) {
     for (int i = 0; i < _POWER; ++i) {
         for (int j = 0; j < _POWER; ++j) {
             std::cout << mult_matrix[i][j];
@@ -111,18 +129,113 @@ Poly Poly::operator+(const Poly& pol) const {
     return res;
 }
 
-//operations
-void Poly::lcycle_shift(std::bitset<_POWER>& b, int i) {
-    b = (b >> i) ^ (b << _POWER - i);
+//Poly Poly::operator*(const Poly& right) const {
+//    std::bitset<_POWER> res_coefs;
+//    
+//    std::bitset<_POWER> left_temp = this->coefficients;
+//    std::bitset<_POWER> lreversed = reverse_bitset(left_temp);
+//
+//    std::bitset<_POWER> right_temp = right.coefficients;
+//    std::bitset<_POWER> rreversed = reverse_bitset(right_temp);
+//    std::bitset<_POWER> temp_mult;
+//
+//    for (int i = 0; i < _POWER; i++,
+//        cycle_shift_to_high(left_temp, 1),
+//        cycle_shift_to_high(right_temp, 1))
+//    {
+//        for (int j = 0; j < _POWER; j++){
+//            temp_mult[j] = get_sum_of_bits(left_temp & mult_matrix[j]);
+//        }
+//
+//        res_coefs[i] = get_sum_of_bits(temp_mult & right_temp);
+//    }
+//
+//    return Poly(res_coefs);
+//}
+
+Poly Poly::operator*(const Poly& right) const {
+    std::bitset<_POWER> res_coefs;
+
+    std::bitset<_POWER> left_temp = this->coefficients;
+    std::bitset<_POWER> lreversed = reverse_bitset(left_temp);
+
+    std::bitset<_POWER> right_temp = right.coefficients;
+    std::bitset<_POWER> rreversed = reverse_bitset(right_temp);
+    std::bitset<_POWER> temp_mult;
+
+    for (int i = 0; i < _POWER; i++) {
+        for (int j = 0; j < _POWER; j++) {
+            temp_mult[j] = get_sum_of_bits(lreversed & mult_matrix[j]);
+            //std::cout << "First mult res: " << temp_mult << std::endl;
+        }
+
+        res_coefs[i] = get_sum_of_bits(temp_mult & rreversed);
+        //std::cout << "Second mult res: " << res_coefs[i] << std::endl;
+        cycle_shift_to_high(lreversed, 1);
+        cycle_shift_to_high(rreversed, 1);
+    }
+    std::bitset<_POWER> res = reverse_bitset(res_coefs);
+    return Poly(res);
 }
 
-void Poly::rcycle_shift(std::bitset<_POWER>& b, int i) {
+//Poly Poly::operator*(const Poly& right) const {
+//    std::bitset<_POWER> res_coefs;
+//    std::bitset<_POWER> u_shifted = this->coefficients;
+//    std::bitset<_POWER> v_shifted = right.coefficients;
+//
+//    std::bitset<_POWER> u = reverse_bitset(u_shifted);
+//    std::bitset<_POWER> v = reverse_bitset(v_shifted);
+//
+//    for (size_t i = 0; i < _POWER; ++i) {
+//        std::cout << "u before sh: " << u << std::endl;
+//        std::cout << "v before sh: " << v << std::endl;
+//        rcycle_shift(u, 1);
+//        rcycle_shift(v, 1);
+//        std::cout << "u after sh: " << u << std::endl;
+//        std::cout << "v after sh: " << v << std::endl;
+//
+//        std::bitset<_POWER> temp_result;
+//        for (size_t j = 0; j < _POWER; ++j) {
+//            temp_result[j] = get_sum_of_bits(u_shifted & mult_matrix[j]) ; 
+//        }
+//        res_coefs[_POWER - i - 1] = get_sum_of_bits(temp_result & v_shifted) ;
+//    }
+//
+//    return Poly(res_coefs);
+//}
+
+//operations
+void Poly::cycle_shift_to_low(std::bitset<_POWER>& b, int i) {  
     b = (b << i) ^ (b >> _POWER - i);
+}
+
+void Poly::cycle_shift_to_high(std::bitset<_POWER>& b, int i) {
+    b = (b >> i) ^ (b << _POWER - i);
 }
 
 
 Poly Poly::square() const {
     Poly res = *this;
-    lcycle_shift(res.coefficients, 1);
+    cycle_shift_to_high(res.coefficients, 1);
     return res;
+}
+
+bool Poly::get_sum_of_bits(const std::bitset<_POWER>& val){
+    bool res = 0;
+    for (int i = 0; i < _POWER; ++i) {
+        res ^= val[i];
+    }
+    return res;
+}
+
+std::bitset<443> reverse_bitset(const std::bitset<443>& bits) {
+    std::bitset<443> reversed;
+    for (size_t i = 0; i < 443; ++i) {
+        reversed[i] = bits[443 - 1 - i];
+    }
+    return reversed;
+}
+
+int Poly::trace(const Poly& poly) {
+    return get_sum_of_bits(poly.coefficients);
 }
--- Poly.h
+++ Poly.h
@@ -31,12 +31,19 @@ public:
 	Poly operator*(const Poly&) const;
 
 	//operations
-	static void lcycle_shift(std::bitset<_POWER>& b, int i);
-    static void rcycle_shift(std::bitset<_POWER>& b, int i);
+	static void cycle_shift_to_low(std::bitset<_POWER>& b, int i);
+    static void cycle_shift_to_high(std::bitset<_POWER>& b, int i);
+	static bool get_sum_of_bits(const std::bitset<_POWER>&);
 	Poly square() const;
+	static int trace(const Poly&);
 
 	//helpers
-	void generate_mult_matrix(static std::bitset<_POWER> mult_matrix[_POWER]);
-	void print_mult_matrix(static std::bitset<_POWER> mult_matrix[_POWER]);
+	static void generate_mult_matrix(static std::bitset<_POWER> mult_matrix[_POWER]);
+	static void print_matrix(static std::bitset<_POWER> mult_matrix[_POWER]);
+	static std::bitset<_POWER>* get_mult_matrix() {
+		return mult_matrix;
+	}
 };
+
+std::bitset<443> reverse_bitset(const std::bitset<443>& bits);
 
